ae40bede5f05348c4419437172d5e48ea1ba809e
max-hold <maxpaney288@gmail.com>
2026-05-17 20:12:27 +0530

    remove the config file genereted form pre_seed_generater

--- TRY-FIREFOX/config/autoconfig.js
+++
@@ -1,5 +0,0 @@
-// Location: /usr/lib/firefox/defaults/pref/autoconfig.js
-// Tells Firefox to load firefox.cfg as its AutoConfig file.
-// Do NOT rename or move this file.
-pref("general.config.filename", "firefox.cfg");
-pref("general.config.obscure_value", 0);
